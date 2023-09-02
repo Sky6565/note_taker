b87f5d3f6e3a68d4649f6c1c890e399df78ed343
sky6565 <godwin1988b@outlook.com>
2023-09-02 10:11:46 -0700

    Created a TODO

---
+++ Develop/server.js
@@ -0,0 +1,18 @@
+// Import Express.js
+// Import built-in Node.js package 'path' to resolve path of files that are located on the server
+// Initialize an instance of Express.js
+// Specify on which port the Express.js server will run
+// Static middleware pointing to the public folder
+
+// Create Express.js routes for default '/', '/send' and '/routes' endpoints (GET request for NOTES)
+//send a message to the client
+//log our request to the terminal
+// POST request to add note
+// Log that a POST request was received
+// Destructuring assignment for the items in req.body
+
+// If all the required properties are present
+// Variable for the object we will save
+// Convert the data to a string so we can save it
+// Write the string to a file
+// Write updated reviews back to the file
